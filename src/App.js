561cb9612dc6b302dae2051618578d46d28bb0b3
Alice Abarca <aliceabarca@otmail.com>
2023-09-19 20:04:14 -0600

    Add use effect to fetch data and add homeview component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
-import logo from './logo.svg';
 import './App.css';
+import { fetchDrinksById } from './api';
+import { useState, useEffect } from 'react';
+// import SearchDrink from './components/SearchDrink/SearchDrink';
+import HomeView from './components/HomeView/HomeView';
 
 function App() {
+  const [drinks, setDrink] = useState([])
+  const [search, setSearch] = useState('')
+
+  useEffect(() => {
+    fetchDrinksById()
+    .then(data => {
+      setDrink(data)
+    })
+    .catch(error => {
+      console.log(`Request failed - ${error.message}`)
+      })
+  }, [])
+
+
   return (
     <div className="App">
-
+      <HomeView />
     </div>
   );
 }

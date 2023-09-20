1d020720905f6f06b3650ae01913a55c8ba93f7c
Alice Abarca <aliceabarca@otmail.com>
2023-09-19 20:05:31 -0600

    Add header component

---
+++ src/components/HomeView/HomeView.js
@@ -0,0 +1,12 @@
+import './HomeView.css'
+import Header from '../Header/Header';
+
+const HomeView = () => {
+  return (
+    <div className='home-view'>
+      <Header />
+    </div>
+  ) 
+}
+
+export default HomeView;

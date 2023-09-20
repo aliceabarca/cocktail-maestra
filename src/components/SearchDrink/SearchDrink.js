9d8943c3bacea0a1c0c087623afc3ae306dd07e2
Alice Abarca <aliceabarca@otmail.com>
2023-09-20 17:50:35 -0600

    Add option elements and remove input text

--- src/components/SearchDrink/SearchDrink.js
+++ src/components/SearchDrink/SearchDrink.js
@@ -1,20 +1,41 @@
 import './SearchDrink.css'
+import { useState } from 'react'
+import { fetchDrinks } from '../../api'
 
-const SearchDrink = ({ setSearch }) => {
+const SearchDrink = () => {
+  const [search, setSearch] = useState('')
+
   const handleSearch = (e) => {
     setSearch(e.target.value)
   }
 
+  const fetchAlcohol = () => {
+    fetchDrinks(search)
+      .then((data) => {
+        console.log(data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
   return (
     <section className='search'>
-      <label form='search-section'></label>
-      <input
-        id='search'
-        className=''
-        type="text"
-        placeholder='Search..'
-        onChange={handleSearch}
-      />
+      <select className='button-src' value={search} onChange={handleSearch}>
+        <option value={null}>Taste Something New...</option>
+        <option value="Whiskey">Whiskey</option>
+        <option value="Brandy">Brandy</option>
+        <option value="Rum">Rum</option>
+        <option value="Vodka">Vodka</option>
+        <option value="Absinthe">Absinthe</option>
+        <option value="Tequila">Tequila</option>
+        <option value="Mezcal">Mezcal</option>
+        <option value="Gin">Gin</option>
+        <option value="Everclear">Everclear</option>
+        <option value="Beer">Beer</option>
+        <option value="Wine">Wine</option>
+      </select>
+      <button onClick={fetchAlcohol}>Fetch Drinks</button>
     </section>
   )
 }

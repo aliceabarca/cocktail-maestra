bf76b8ba8fe2a5a68ad931dc133c2dfe681a491c
Alice Abarca <aliceabarca@otmail.com>
2023-09-20 17:48:20 -0600

    Add destructuring for set search from search drinks component

--- src/components/HomeView/HomeView.js
+++ src/components/HomeView/HomeView.js
@@ -1,10 +1,14 @@
 import './HomeView.css'
 import Header from '../Header/Header';
+import SearchDrink from '../SearchDrink/SearchDrink';
+import { useState } from 'react';
 
-const HomeView = () => {
+const HomeView = ({ setSearch }) => {
+
   return (
     <div className='home-view'>
       <Header />
+      <SearchDrink setSearch={setSearch}/>
     </div>
   ) 
 }

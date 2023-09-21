a25637e6a46defbac042486f9c12fdad16436aae
Alice Abarca <aliceabarca@otmail.com>
2023-09-20 20:49:36 -0600

    Add destructor to home view function and passing search drink component

--- src/components/HomeView/HomeView.js
+++ src/components/HomeView/HomeView.js
@@ -3,11 +3,11 @@ import Header from '../Header/Header';
 import SearchDrink from '../SearchDrink/SearchDrink';
 import { useState } from 'react';
 
-const HomeView = ({ setSearch }) => {
+const HomeView = ({ setSearch, fetchAlcohol }) => {
   return (
     <div className='home-view'>
       <Header />
-      <SearchDrink setSearch={setSearch}/>
+      <SearchDrink setSearch={setSearch} fetchAlcohol={fetchAlcohol}/>
     </div>
   ) 
 }

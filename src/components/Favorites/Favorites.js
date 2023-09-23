fdb3cc415962ca0aee3b22e239b8eed9f13529e6
Alice Abarca <aliceabarca@otmail.com>
2023-09-22 20:00:30 -0600

    Add image to display on favorite route

--- src/components/Favorites/Favorites.js
+++ src/components/Favorites/Favorites.js
@@ -5,6 +5,7 @@ const Favorites = ({name, image, deleteFavorite, id}) => {
   return (
     <div className='favorites'>
       <h3>{name}</h3>
+      <img src={image}></img>
       <button className='delete-button' onClick={() => deleteFavorite(id)}>🗑️</button>
     </div>
   )
